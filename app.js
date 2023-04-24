8f8df32b3c58cb95d55e11a9f39b4e05d7c0e42b
nmt3 <nmttkm3@gmail.com>
2023-04-24 09:27:07 +0000

    fix tab

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let user_hand = prompt("じゃんけんの手を手をグー、チョキ、パーから選んでください");
-while((user_hand!="グー")&&(user_hand!="チョキ")&&(user_hand!="パー")&&(user_hand!=null)){
+while((user_hand != "グー")&&(user_hand != "チョキ")&&(user_hand != "パー")&&(user_hand != null)){
   alert("グー、チョキ、パーのいずれかを入力してください");
    user_hand = prompt("じゃんけんの手を手をグー、チョキ、パーから選んでください");
 };
@@ -33,27 +33,27 @@ function winLose(user,js){
   let winLoseStr;
 
   if(user == "グー"){
-    if(js=="グー"){
+    if(js == "グー"){
       winLoseStr = "あいこ";
-    } else if (js=="チョキ"){
+    } else if (js == "チョキ"){
       winLoseStr = "勝ち";
-    } else if (js=="パー"){
+    } else if (js == "パー"){
       winLoseStr = "負け";
     }
   } else if(user == "チョキ"){
-    if(js=="グー"){
+    if(js == "グー"){
       winLoseStr = "負け";
-    } else if (js=="チョキ"){
+    } else if (js == "チョキ"){
       winLoseStr = "あいこ";
-    } else if (js=="パー"){
+    } else if (js == "パー"){
       winLoseStr = "勝ち";
     }
   } else if(user == "パー"){
-    if(js=="グー"){
+    if(js == "グー"){
       winLoseStr = "勝ち";
-    } else if (js=="チョキ"){
+    } else if (js == "チョキ"){
       winLoseStr = "負け";
-    } else if (js=="パー"){
+    } else if (js == "パー"){
       winLoseStr = "あいこ";
     }
 }
